2fffa794a84e7890570476ebca9a1cf01e799a36
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-03-13 14:37:09 +0900

    Add files via upload

---
+++ 변수출력하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    char b = 'C';
+
+    printf("%d...%c", a, b);
+
+    return 0;
+}
---
+++ 변수출력하기2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char a = 'C';
+    int b = 3;
+
+    printf("%c!.....!%d", a, b);
+
+    return 0;
+}
